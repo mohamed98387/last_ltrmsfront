8f1c04c5b6379bc9da9f2579d47ef85130234520
mohamed98387 <mohamed.lajmi@esprit.tn>
2023-11-23 11:14:59 +0100

    last version23-11-2023

--- src/app/pages/Planification/hebdomadaire/hebdomadaire.component.ts
+++ src/app/pages/Planification/hebdomadaire/hebdomadaire.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
+import { AccountService } from "src/app/services/account.service";
+import jwt_decode from "jwt-decode";
+import { RoleService } from "src/app/services/role.service";
 @Component({
   selector: "app-hebdomadaire",
   templateUrl: "./hebdomadaire.component.html",
@@ -21,9 +24,46 @@ export class HebdomadaireComponent implements OnInit {
       PlantSection: "PS1",
     },
   ];
-  constructor() {}
+  token: string | null;
+  role: string | null;
+  profileinformation: any;
+  roleTable:any[]=[];
+  matchingRole:any;
+  constructor(private accountService:AccountService,private roleService:RoleService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.token = localStorage.getItem("token");
+    if (this.token) {
+      // Decode the JWT token
+      const decodedToken: any = jwt_decode(this.token);
+  
+      // Access the data in the token
+      this.role = decodedToken.roles;
+  
+      // Print the extracted data
+      console.log(this.role[0]);
+  
+      this.roleService.getRoles().subscribe((res) => {
+        console.log(res);
+        
+        // Filter the roleTable array to find the object with roleName equal to this.role[0]
+        this.matchingRole = res.find(role => role.roleName === this.role[0]);
+        
+        // Check if a matching role was found
+        if (this.matchingRole) {
+   
+          console.log("Matching Role:", this.matchingRole);
+          
+        } else {
+          console.log("No matching role found.");
+        }
+        console.log(this.matchingRole.permissions)
+      });
+    }
+}
+hasPermission(permissions: any[], description: string): boolean {
+  return permissions.some(permission => permission.description === description);
+}
   exportToExcel(data: any[], fileName: string) {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
     const workbook: XLSX.WorkBook = {
